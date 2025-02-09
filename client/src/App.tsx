fe9da619bf4cf0e723f7367b8b242fa00169111c
Arinah Karim <ankarim01@gmail.com>
2025-02-09 15:54:44 -0600

    added background image, formatted login screen

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,8 @@
-import { useEffect } from 'react'
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { Login } from './pages/Login';
-import { Container } from 'react-bootstrap';
+import { useEffect } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Login } from "./pages/Login";
+import { Container } from "react-bootstrap";
+import backgroundImage from "./assets/background.svg";
 
 function App() {
   useEffect(() => {
@@ -13,26 +14,35 @@ function App() {
       },
       body: JSON.stringify({ name: "test", email: "test@gmail.com" }),
     })
-    .then((res) => res.json())
-    .then((data) => {
-      console.log("Login Response:", data);
-      return fetch("http://localhost:8000/api/dogs/breeds", {
-        method: "GET",
-        credentials: "include", 
-      });
-    })
-    .then((res) => res.json())
-    .then((anotherData) => {
-      console.log("Another Endpoint Data:", anotherData);
-    })
-    .catch((err) => console.error("Error:", err));
+      .then((res) => res.json())
+      .then((data) => {
+        console.log("Login Response:", data);
+        return fetch("http://localhost:8000/api/dogs/breeds", {
+          method: "GET",
+          credentials: "include",
+        });
+      })
+      .then((res) => res.json())
+      .then((anotherData) => {
+        console.log("Another Endpoint Data:", anotherData);
+      })
+      .catch((err) => console.error("Error:", err));
   }, []);
 
   return (
-    <Container fluid className="vh-100">
+    <Container
+      fluid
+      className="vh-100"
+      style={{
+        backgroundImage: `url(${backgroundImage})`,
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+        height: "100vh",
+      }}
+    >
       <Login />
     </Container>
-  )
+  );
 }
 
-export default App
+export default App;
--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -1,29 +1,27 @@
-import { Button, Col, Form, Row } from "react-bootstrap"
+import { Button, Card, Col, Form, Row, Stack } from "react-bootstrap";
 
 export const Login = () => {
-    return (
-        <Row className="h-100">
-            <Col xs={6}>
-                    <Form className="w-100 d-flex flex-column justify-content-center align-items-center"> 
-            <Form.Group className="mb-3" controlId="formBasicEmail">
-                <Form.Label>Email address</Form.Label>
-                <Form.Control type="email" placeholder="Enter email" />
-            </Form.Group>
-
-            <Form.Group className="mb-3" controlId="formName">
+  return (
+    <Row className="h-100 justify-content-center align-items-center flex-column">
+      <Col xs={4} className="">
+        <Card className="p-5" bg="light">
+          <Form className="w-100">
+            <Stack gap={3}>
+              <Form.Group controlId="formName">
                 <Form.Label>Name</Form.Label>
                 <Form.Control type="text" placeholder="Enter name" />
-            </Form.Group>
-
-            <Button variant="primary" type="submit">
+              </Form.Group>
+              <Form.Group controlId="formBasicEmail">
+                <Form.Label>Email address</Form.Label>
+                <Form.Control type="email" placeholder="Enter email" />
+              </Form.Group>
+              <Button variant="primary" type="submit">
                 Submit
-            </Button>
-      </Form>
-            </Col>
-            <Col xs={6} className="bg-primary">
-                <div className="bg-primary" />
-            </Col>
-        </Row>
-
-    )
-}
+              </Button>
+            </Stack>
+          </Form>
+        </Card>
+      </Col>
+    </Row>
+  );
+};
